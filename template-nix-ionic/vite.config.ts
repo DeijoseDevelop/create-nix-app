dbb6053685298991e3a504e9fefd898889e1fe23
Jose Vasquez <tech@zekrost.com>
2026-06-22 20:21:45 -0500

    chore: include @deijose/vite-plugin-nix-js in all Vite templates

--- template-nix-ionic/vite.config.ts
+++ template-nix-ionic/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import nix from "@deijose/vite-plugin-nix-js";
 
 export default defineConfig({
+  plugins: [nix()],
   build: {
     rollupOptions: {
       output: { manualChunks: undefined },
--- template-vite-ts/vite.config.ts
+++ template-vite-ts/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
+import nix from "@deijose/vite-plugin-nix-js";
 
 export default defineConfig({
-    plugins: [],
+    plugins: [nix()],
 });
